e31645d3cca68afeabfec3389e1259882bd44436
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2024-11-26 21:33:18 -0500

    put modal in a web component

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@ import "./accessibility.js";
 import "./header.js";
 import "./nav.js";
 import "./footer.js";
-import "./modal.js";
 import "./swiper.js";
 import "./copyright.js";
+import "./modal.js";
 import "./mapbox.js";
--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,3 +1,52 @@
+class Modal extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <!-- Modal HTML -->
+    <div id="myModal" class="modal">
+      <div class="modal__content">
+        <span class="close">&times;</span>
+        <h1>Interested in joining us at Honeygo Pediatrics?</h1>
+        <p>Please give us a call today!</p>
+        <p>
+          <a class="in-text-link" href="tel:4109338101">410-933-8101</a>
+        </p>
+        <div class="modal__options">
+          <a href="/new-patients.html">
+            <i class="fa-regular fa-face-smile service-icon"></i>
+            <p>We're accepting new patients</p>
+          </a>
+          <a href="/immunizations-and-shots.html">
+            <i class="fa-solid fa-syringe service-icon"></i>
+            <p class="service-p mont">
+              We offer updated COVID-19 vaccinations and Flu vaccinations
+            </p>
+          </a>
+          <a href="virtual-medical-services.html">
+            <i class="fa-solid fa-house-laptop service-icon"></i>
+            <p>Ask us about telehealth visits!</p>
+          </a>
+          <a href="/contact.html">
+            <i class="fa-solid fa-thermometer service-icon"></i>
+            <p class="service-p mont">
+              Walk in hours for sick patients with acute / urgent care needs
+            </p>
+            <p class="service-p mont">
+              <strong> 9 AM - 10 AM Monday - Friday </strong>
+            </p>
+            <p class="service-p mont">
+              <strong> 9 AM - 10 AM Saturdays </strong>
+            </p>
+          </a>
+        </div>
+      </div>
+    </div>
+
+        `;
+  }
+}
+
+customElements.define("hg-modal", Modal);
+
 // Check if the modal has already been displayed in this session
 const modalDisplayed = sessionStorage.getItem("modalDisplayed");
 const lastModalTime = sessionStorage.getItem("lastModalTime");

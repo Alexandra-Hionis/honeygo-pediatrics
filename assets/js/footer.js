e1a01fde197ee55bb41ee562f85e2f7ff0ef5c01
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2025-01-15 22:38:54 -0700

    service announcement

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,6 +1,19 @@
 class Footer extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
+    <input type="checkbox" id="announcement-toggle" class="announcement-toggle">
+    <section class="announcement gradient-gray">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M192 0c-41.8 0-77.4 26.7-90.5 64L64 64C28.7 64 0 92.7 0 128L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64l-37.5 0C269.4 26.7 233.8 0 192 0zm0 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM112 192l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg>
+        <div class="announcement-content">
+        <label for="announcement-toggle" class="close-btn">&times;</label>
+        <h2>Service Announcement</h2>
+        <p>
+            We now offer comprehensive evaluation, treatment, and ongoing care for
+            behavioral issues and ADHD, along with a pediatric weight loss
+            management program.
+        </p>
+        </div>
+    </section>
     <!-- Footer -->
     <section class="to-top-container">
     <div class="to-top">
